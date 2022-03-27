f0d6db17b42d109e12fd273c8f1b3ded4b2ea06f
ALXKTZ1 <96651467+ALXKTZ1@users.noreply.github.com>
2022-03-27 15:18:12 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,13 @@
 Mbit_IR.onPressEvent(RemoteButton.Down, function () {
     Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Back, 100)
     basic.pause(100)
+    Tinybit.CarCtrl(Tinybit.CarState.Car_Stop)
+})
+Mbit_IR.onPressEvent(RemoteButton.Beep, function () {
+    music.playMelody("A B A G F F - - ", 120)
+})
+Mbit_IR.onPressEvent(RemoteButton.Power, function () {
+    Tinybit.CarCtrl(Tinybit.CarState.Car_Stop)
 })
 Mbit_IR.onPressEvent(RemoteButton.One, function () {
     Tinybit.RGB_Car_Big(Tinybit.enColor.Red)
@@ -14,8 +21,9 @@ Mbit_IR.onPressEvent(RemoteButton.Three, function () {
 Mbit_IR.onPressEvent(RemoteButton.Up, function () {
     Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 100)
     basic.pause(100)
+    Tinybit.CarCtrl(Tinybit.CarState.Car_Stop)
 })
-Mbit_IR.init(Pins.P8)
+Mbit_IR.init(Pins.P9)
 basic.forever(function () {
     basic.showLeds(`
         . . . . .
